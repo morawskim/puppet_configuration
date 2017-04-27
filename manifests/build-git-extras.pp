655bdb5c8d605df0d5c77cc492ff6caaff163551
Marcin Morawski <marcin@morawskim.pl>
2017-04-27 20:17:35 +0200

    manifest for build git-extras rpm package

---
+++ manifests/build-git-extras.pp
@@ -0,0 +1,41 @@
+node default {
+
+    include mopensuse::zypper::refresh
+    include mopensuse::zypper::repositories::ruby_extensions
+    include mopensuse::user::rpm
+    include mopensuse::packages::rpmbuild
+
+    $rpm_home=$::mopensuse::user::rpm::user_home_path
+    $rpmbuild_top="${rpm_home}/rpmbuild"
+
+    exec { 'zypper_update':
+        command => '/usr/bin/zypper --non-interactive --no-color update',
+        require => Class['mopensuse::zypper::refresh']
+    }
+
+    package {['rubygem-ronn', 'git']:
+        ensure  => present,
+        require => Class['mopensuse::zypper::repositories::ruby_extensions']
+    }
+
+    vcsrepo { $rpmbuild_top:
+        ensure   => present,
+        provider => git,
+        source   => 'https://github.com/morawskim/rpmbuild.git',
+        revision => "openSUSE_${::operatingsystemrelease}",
+        owner    => 'rpm',
+        group    => 'users',
+        require => [ Class['mopensuse::user::rpm'], Package['git'] ]
+    }
+
+    # $facts['vagrant'] Only works with Puppet 3.5 or later.
+    # Disabled by default in open source releases prior to Puppet 4.0.
+    if $::vagrant {
+        file {'/vagrant':
+            ensure  => directory,
+            mode    => '1775',
+            owner   => 'vagrant',
+            group   => 'users',
+        }
+    }
+}
