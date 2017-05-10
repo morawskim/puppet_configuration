11cf307a71c6811a447326efeaa618c88b836905
Marcin Morawski <marcin@morawskim.pl>
2017-05-10 20:03:15 +0200

    fix build errors

--- manifests/rpm/python-mitmproxy-bundled.pp
+++ manifests/rpm/python-mitmproxy-bundled.pp
@@ -5,13 +5,26 @@ node default {
     include mopensuse::packages::rpmbuild
     include mopensuse::packages::vcs
     include mopensuse::zypper::repositories::devel-languages-python
+    include mopensuse::zypper::repositories::server_monitoring
 
     $rpm_home=$::mopensuse::user::rpm::user_home_path
     $rpmbuild_top="${rpm_home}/rpmbuild"
 
 
-    package {['python-base', 'python-setuptools']:
-        ensure  => present
+    package {['python-base', 'python-setuptools',
+      'python-devel', 'python-pip',
+      'python-Sphinx', 'python-sphinxcontrib-documentedlist']:
+        ensure  => present,
+        require => Class['mopensuse::zypper::repositories::devel-languages-python']
+    }
+
+    package {'libffi-devel':
+        ensure  => present,
+        require => Class['mopensuse::zypper::repositories::server_monitoring']
+    }
+
+    package {['libopenssl-devel', 'libxml2-devel', 'libxslt-devel']:
+        ensure  => present,
     }
 
     vcsrepo { $rpmbuild_top:
