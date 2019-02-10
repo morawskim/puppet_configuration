0bcec0c09ad6376069e04c34eed694de94286171
Marcin Morawski <marcin@morawskim.pl>
2019-02-10 14:59:59 +0100

    suse15 manifest - Add only required repositories

--- manifests/suse15.pp
+++ manifests/suse15.pp
@@ -1,25 +1,41 @@
 node default {
    include mopensuse::zypper::refresh
+#    include mopensuse::services::snapd
 
    class {'firewalld':
    }
 
-   class {'ca_cert':
-   }
+#    class {'ca_cert':
+#    }
 
    $rpmkeys = lookup(mopensuse::rpmkeys)
-   $rpmkeys.each |String $key, Hash $attrs| {
-       rpmkey {$key:
-           * => $attrs
-       }
-   }
+   rpmkey {'devel_tools': * => $rpmkeys['devel_tools']}
+   rpmkey {'google_chrome': * =>  $rpmkeys['google_chrome']}
+   rpmkey {'kde_extra': * => $rpmkeys['kde_extra']}
+   rpmkey {'morawskim': * =>  $rpmkeys['morawskim']}
+   rpmkey {'obs_morawskim': * => $rpmkeys['obs_morawskim']}
+   rpmkey {'packman': * => $rpmkeys['packman']}
+   rpmkey {'security_privacy': * => $rpmkeys['security_privacy']}
+   rpmkey {'skype': * => $rpmkeys['skype']}
+   rpmkey {'system_snappy': * => $rpmkeys['system_snappy']}
+   rpmkey {'utilities': * => $rpmkeys['utilities']}
+   rpmkey {'vivaldi': * => $rpmkeys['vivaldi']}
+   rpmkey {'yarn': * => $rpmkeys['yarn']}
 
    $zypprepos = lookup(mopensuse::zypprepos, {merge => deep})
-   $zypprepos.each |String $key, Hash $attrs| {
-       zypprepo {$key:
-           * => $attrs
-       }
-   }
+   zypprepo { 'devel_tools': * => $zypprepos['devel_tools']}
+   zypprepo { 'google_chrome': * => $zypprepos['google_chrome']}
+   zypprepo { 'kde_extra': * => $zypprepos['kde_extra']}
+   zypprepo { 'morawskim': * => $zypprepos['morawskim']}
+   zypprepo { 'obs_morawskim': * => $zypprepos['obs_morawskim']}
+   zypprepo { 'packman': * => $zypprepos['packman']}
+   zypprepo { 'security_privacy': * => $zypprepos['security_privacy']}
+   zypprepo { 'skype_stable': * => $zypprepos['skype_stable']}
+   zypprepo { 'system_snappy': * => $zypprepos['system_snappy']}
+   zypprepo { 'utilities': * => $zypprepos['utilities']}
+   zypprepo { 'vivaldi': * => $zypprepos['vivaldi']}
+   zypprepo { 'yarn': * => $zypprepos['yarn']}
+
 
    $packages = lookup(mopensuse::packages,  {merge => deep})
    $packages.each |String $key, Hash $attrs| {
@@ -28,19 +44,24 @@ node default {
        }
    }
 
-   $printers = lookup(mopensuse::printers)
-   $printers.each |String $key, Hash $attrs| {
-       printer {$key:
-           * => $attrs
-       }
-   }
-
-   default_printer { lookup(mopensuse::default_printer):
-    ensure  => present,
-  }
+#    $printers = lookup(mopensuse::printers)
+#    $printers.each |String $key, Hash $attrs| {
+#        printer {$key:
+#            * => $attrs
+#        }
+#    }
+#
+#    default_printer { lookup(mopensuse::default_printer):
+#     ensure  => present,
+#   }
 
     Rpmkey<||> -> Zypprepo<||> ~> Class['mopensuse::zypper::refresh']
     Zypprepo<||> -> Package<||>
     Package<||> -> Service<||>
+    File <| ensure == 'dir' |> -> File <| ensure == 'present' |>
     Printer<||> -> Default_printer<||>
+
+    Package<|title == 'postfix'|> -> Package<|title == 'nullmailer'|>
+    Package<|title == 'rsyslog'|> -> Package<|title == 'systemd-logger'|>
+    Package<|title == 'kdeconnect-kde'|> -> Firewalld_service<|title == 'kdeconnect-kde'|>
 }
